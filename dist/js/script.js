eeb7030b21601c884f794d98af5bd7453d72f7c6
Timur <Timur5414175!>
2022-08-31 22:44:49 +0500

    added adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-    $('.carousel').slick({
-      slidesToShow: 3,
-      variableWidth: true,
-      centerMode: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow/arrow-left.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow/arrow-right.png"></button>',
-    });
+  $('.carousel').slick({
+    slidesToShow: 3,
+    variableWidth: true,
+    centerMode: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow/arrow-left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow/arrow-right.png"></button>',
   });
+
+    // Smooth scroll and pageup
+  $(window).scroll(function () {
+    if($(this).scrollTop() > 800){
+      $('.pageup').fadeIn();
+    } else{
+      $('.pageup').fadeOut();
+    }
+  });
+
+});
